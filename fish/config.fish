c192be83e820dfabc5df00f6e211d6f5664c2818
Fabian Spataru <fabian.spataru2@f-i-ts.de>
2024-02-22 12:14:07 +0100

    Updated Configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 	starship init fish | source
+	zoxide init fish | source
 
 	# some more ls aliases
 	alias ll='ls -alF'
